154412062003ac18480705d647f0c7b30035eb64
HisakadoMotoki <sagamihara71@gmail.com>
2020-04-06 20:00:08 +0900

    add message & add index action view

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,19 +1,32 @@
 class PicturesController < ApplicationController
+before_action :find_pic, only: [:show, :edit, :update, :destroy]
 
   def index
+    @pic = Picture.all
   end
   def new
-    @picture = Picture.new
+    @pic = Picture.new
   end
 
   def create
-    @picture = Picture.new(pic_params)
+    @pic = Picture.new(pic_params)
+    if @pic.save
+      redirect_to @pic, notice: "投稿されました"
+    else
+      render 'new'
+    end
+  end
+  def show
   end
 
   private
 
   def pic_params
     params.require(:picture).permit(:title, :description)
+  end
+
+  def find_pic
+    @pic = Picture.find(params[:id])
   end
 
 

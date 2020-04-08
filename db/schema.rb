69ea5d26b79403938c38a2a2cd56ad12d65a3879
HisakadoMotoki <sagamihara71@gmail.com>
2020-04-08 16:15:44 +0900

    complete to resize image to arrange css

--- db/schema.rb
+++ db/schema.rb
@@ -18,16 +18,12 @@ ActiveRecord::Schema.define(version: 2020_04_07_101232) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "user_id"
-    t.string "image_file_name"
-    t.string "image_content_type"
-    t.bigint "image_file_size"
-    t.datetime "image_updated_at"
     t.string "image"
     t.index ["user_id"], name: "index_pictures_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
+    t.string "email"
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
@@ -40,6 +36,7 @@ ActiveRecord::Schema.define(version: 2020_04_07_101232) do
     t.string "link"
     t.string "tel"
     t.integer "sex"
+    t.string "image"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end

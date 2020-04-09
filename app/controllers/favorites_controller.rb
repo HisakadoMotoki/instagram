2a81b3dc9a9446ab4d1361dedd06556232122dbc
HisakadoMotoki <sagamihara71@gmail.com>
2020-04-09 17:03:34 +0900

    move fav function to pic-show view

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,12 +2,12 @@ class FavoritesController < ApplicationController
   def create
     favorite = current_user.favorites.build(picture_id: params[:picture_id])
     favorite.save
-    redirect_to pictures_path
+    redirect_to picture_path(params[:picture_id])
   end
 
   def destroy
     favorite = Favorite.find_by(picture_id: params[:picture_id], user_id: current_user.id)
     favorite.destroy
-    redirect_to pictures_path
+    redirect_to picture_path(params[:picture_id])
   end
 end

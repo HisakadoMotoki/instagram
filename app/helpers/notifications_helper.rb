7c33c9bb4f4aef40363fd47418e68b918f81950d
motoki <sagamihara71@gmail.com>
2020-06-04 01:57:50 +0900

    通知機能のデバッグ

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -3,7 +3,7 @@ module NotificationsHelper
   def notification_form(notification)
   @comment = nil
   visitor = link_to notification.visitor.name, notification.visitor, style:"font-weight: bold;"
-  your_picture = link_to 'あなたの投稿', notification.picture, style:"font-weight: bold;", remote: true
+  your_picture = link_to 'あなたの投稿', notification.picture, style:"font-weight: bold;"
   case notification.action
     when "follow" then
       "#{visitor}があなたをフォローしました"

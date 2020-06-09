73aae4e57ad67b29ffc3fb2fc19c3fd47124ebd4
hisakadomotoki <sagamihara71@gmail.com>
2020-06-09 09:49:08 +0900

    debug notification

--- app/helpers/notifications_helper.rb
+++ app/helpers/notifications_helper.rb
@@ -3,14 +3,13 @@ module NotificationsHelper
   def notification_form(notification)
   @comment = nil
   visitor = link_to notification.visitor.name, notification.visitor, style:"font-weight: bold;"
-  your_picture = link_to 'あなたの投稿', notification.picture, style:"font-weight: bold;", remote: true
+  your_picture = link_to 'あなたの投稿', notification.picture, style:"font-weight: bold;"
   case notification.action
     when "follow" then
       "#{visitor}があなたをフォローしました"
     when "favorite" then
       "#{visitor}が#{your_picture}にいいね！しました"
     when "comment" then
-      #@comment = Comment.find_by(id:notification.comment_id)&.content
       "#{visitor}が#{your_picture}にコメントしました"
     end
   end
